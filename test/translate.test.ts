589a2fb3704f3374f43ac73d18c898053263f599
林楠 <282712@nd.com>
2020-07-21 15:55:36 +0800

    test: 对象翻译单元测试

--- test/translate.test.ts
+++ test/translate.test.ts
@@ -1,4 +1,6 @@
-import { translate } from '../src'
+import { keys, values, forEach } from 'lodash'
+import { translate, translateObject } from '../src'
+import { includeHan } from '../src/utils/detecter'
 
 describe('translate core', () => {
   it('translate text correct', (done) => {
@@ -23,10 +25,44 @@ describe('translate core', () => {
     })
   })
 
-  it('translate invalid text return undefined', (done) => {
-    translate('').then((translatedText) => {
-      expect(translatedText).toEqual('')
+  it('translate text not including Han character.', (done) => {
+    translate('test').then((translatedText) => {
+      expect(typeof translatedText).toEqual('string')
+      expect(translatedText).toEqual('test')
       done()
+    })
+  })
+
+  it('translate invalid text return undefined', async () => {
+    const translatedText = await translate('')
+    expect(translatedText).toEqual('')
+  })
+
+  it('translate object correct', async () => {
+    const targetObject = {
+      "删除成功": "删除成功",
+      "新增成功": "新增成功",
+      "学科": "学科",
+      "模板": "模板",
+      "标签": "标签",
+      "FooZhower": "FooZhower"
+    }
+
+    const translatedObjectResults = await translateObject(targetObject)
+
+    expect(keys(translatedObjectResults).length).toEqual(keys(targetObject).length)
+
+    forEach(values(translatedObjectResults), (result) => {
+      expect(includeHan(result as string)).toBeFalsy()
+    })
+  })
+
+  it('translate illegal object, return empty object', async () => {
+    const illegalObjects = [null, undefined, '', 1, NaN]
+
+    illegalObjects.forEach(async (illegalObject) => {
+      const translatedResult = await translateObject(illegalObject as any)
+      expect(translatedResult).toEqual({})
     })
   })
 })
