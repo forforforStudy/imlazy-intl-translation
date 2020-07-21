841b6532a8ffae658494f447f78d0d1e10f5b07d
林楠 <282712@nd.com>
2020-07-21 15:03:06 +0800

    test: 加入基础单元测试 jest 框架

---
+++ test/translate.test.ts
@@ -0,0 +1,32 @@
+import { translate } from '../src'
+
+describe('translate core', () => {
+  it('translate text correct', (done) => {
+    const beginTime = Date.now()
+    const text = '苹果'
+    translate(text).then((translatedText) => {
+      const firstTransTime = Date.now()
+      expect(translatedText).toEqual('Apple')
+
+      /**
+       * 命中缓存
+       */
+      translate(text).then((translatedText) => {
+        const cacheTransTime = Date.now()
+        expect(translatedText).toEqual('Apple')
+        /**
+         * 命中缓存的时间应该比第一次获取小一个数据量级
+         */
+        expect(((cacheTransTime - firstTransTime) * 10) < (firstTransTime - beginTime) ).toBeTruthy()
+        done()
+      })
+    })
+  })
+
+  it('translate invalid text return undefined', (done) => {
+    translate('').then((translatedText) => {
+      expect(translatedText).toBeUndefined()
+      done()
+    })
+  })
+})
